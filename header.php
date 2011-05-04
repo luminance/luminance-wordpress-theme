e43dcd282f45fc248a66e6bf90b5b2a727b86193
Jodok Batlogg <jodok@batlogg.com>
2011-05-04 21:51:02 +0200

    fixed broken links, added luminance favicon

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <!-- the "no-js" class is for Modernizr. -->
 
-<head id="www-sitename-com" data-template-set="html5-reset-wordpress-theme" profile="http://gmpg.org/xfn/11">
+<head id="www-luminance-tv" data-template-set="html5-reset-wordpress-theme" profile="http://gmpg.org/xfn/11">
 
 	<meta charset="<?php bloginfo('charset'); ?>">
 	
@@ -58,17 +58,20 @@
 		         echo ' - page '. $paged; }
 		   ?>">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
-	
-	<meta name="google-site-verification" content="">
-	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
-	
-	<meta name="author" content="Your Name Here">
-	<meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
+<?php	
+# we're using dns verification, so no need for that block
+#	<meta name="google-site-verification" content="">
+#	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
 
-	<!-- Dublin Core Metadata : http://dublincore.org/ -->
-	<meta name="DC.title" content="Project Name">
-	<meta name="DC.subject" content="What you're about.">
-	<meta name="DC.creator" content="Who made this site.">
+# no meta data so far. google doesn't use them at all. 
+#	<meta name="author" content="Your Name Here">
+#	<meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
+
+#	<!-- Dublin Core Metadata : http://dublincore.org/ -->
+#	<meta name="DC.title" content="Project Name">
+#	<meta name="DC.subject" content="What you're about.">
+#	<meta name="DC.creator" content="Who made this site.">
+	?>
 	
 	<!--  Mobile Viewport meta tag
 	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
@@ -79,13 +82,13 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 	-->
 	
-	<link rel="shortcut icon" href="<?php echo TEMPLATEPATH; ?>/_/img/favicon.ico">
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
 	<!-- This is the traditional favicon.
 		 - size: 16x16 or 32x32
 		 - transparency is OK
 		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
 		 
-	<link rel="apple-touch-icon" href="<?php echo TEMPLATEPATH; ?>/_/img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
 	<!-- The is the icon for iOS's Web Clip.
 		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
 		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
@@ -95,7 +98,7 @@
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	
 	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
-	<script src="<?php echo TEMPLATEPATH; ?>/_/js/modernizr-1.7.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
