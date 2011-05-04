3b707eb4af01487f72e7225e7bfe796c87e09e8c
Matthias Kampitsch <matthias@starflower.at>
2011-05-04 09:48:03 +0200

    php refine
    Signed-off-by: Matthias Kampitsch <matthias@starflower.at>

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
 <?php
-if ( is_home() ) {
      if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<?php 
-				
-				echo get_the_post_thumbnail($id, 'full', array(
+				if(has_post_thumbnail()) {
+					echo get_the_post_thumbnail($id, 'full', array(
 															
 															'class'	=> "attachment-$size",
 															'alt'	=> trim(strip_tags( $post->post_title )),
 															'title'	=> trim(strip_tags( $post->post_title )),
 														)); 
+				}
 			?>
 			
 
@@ -33,29 +33,6 @@ if ( is_home() ) {
 
 		<h2>Not Found</h2>
 
-	<?php endif; 
-
-} else {?>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-
-			<div class="entry">
-				<?php the_content(); ?>
-			</div>
-
-		</article>
-
-	<?php endwhile; ?>
-
-	<?php else : ?>
-
-		<h2>Not Found</h2>
-
-	<?php endif;
-
-}
-?>
+	<?php endif; ?>
 
 <?php get_footer(); ?>
